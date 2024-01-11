2a0b3ff335a722847fb36e954d73f07200ce852f
solidados <geekbrains.pk@internet.ru>
2024-01-11 23:51:38 +0400

    feat: pass props to MovieCard of fetchedData as movie

--- src/components/movieCard/MovieCard.jsx
+++ src/components/movieCard/MovieCard.jsx
@@ -8,27 +8,27 @@ import MoviePoster from "./ui/MoviePoster.jsx";
 // styles
 import './styles/movieCard.scss'
 
-const MovieCard = () => {
+const MovieCard = ({ movie }) => {
   const IMG_URL = 'https://image.tmdb.org/t/p/w200';
   const { movieData, isDescriptionVisible, handleMouseEnter, handleMouseLeave } = useMovieData();
 
-  if (!movieData) {
+  if (!movie) {
     return <div>Loading...</div>;
   }
 
   return (
     <div className="card" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
       <div className="card-poster">
-        <MoviePoster moviePath={`${IMG_URL}${movieData.poster_path}`} />
-        <MovieVote voteAverage={movieData.vote_average} />
+        <MoviePoster moviePath={`${IMG_URL}${movie.poster_path}`} />
+        <MovieVote voteAverage={movie.vote_average} />
       </div>
-      {isDescriptionVisible && (
-        <MovieDescription
-          title={movieData.title}
-          tagline={movieData.tagline}
-          isVisible={isDescriptionVisible}
-        />
-      )}
+      {/*{isDescriptionVisible && (*/}
+      {/*  <MovieDescription*/}
+      {/*    title={movie.title}*/}
+      {/*    overview={movie.overview}*/}
+      {/*    isVisible={isDescriptionVisible}*/}
+      {/*  />*/}
+      {/*)}*/}
     </div>
   );
 };
