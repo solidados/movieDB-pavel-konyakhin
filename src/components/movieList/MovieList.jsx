f9658965cf3028f55abefb591b2dc055006fac83
solidados <geekbrains.pk@internet.ru>
2024-01-11 23:48:48 +0400

    refactor: minor changes to MovieList

---
+++ src/components/movieList/MovieList.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import { fetchedData } from "../../helpers/api.js";
+import MovieCard from "../movieCard/MovieCard.jsx";
+
+import './movieList.scss'
+
+const MovieList = () => {
+  const [moviesData, setMoviesData] = useState([])
+
+  useEffect(() => {
+    const fetchMovieData = async () => {
+      try {
+        const data = await fetchedData()
+        setMoviesData(data.results)
+      }
+      catch (err) {
+        console.error('Error fetching movie data:', err);
+      }
+    }
+
+    fetchMovieData().catch(err => new Error(err.message))
+  }, []);
+
+  return (
+    <>
+      <h1>Popular Movies</h1>
+      <div className="movie-list">
+        {moviesData.map(movie => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default MovieList;
