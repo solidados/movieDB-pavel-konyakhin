86e1baefd5b862f0ddaa4ccc7ab85a5560409f9c
solidados <geekbrains.pk@internet.ru>
2024-01-12 22:14:24 +0400

    feat: FavoritePage.jsx checks localStorage and render MovieCards on page

--- src/pages/FavoritePage.jsx
+++ src/pages/FavoritePage.jsx
@@ -1,6 +1,29 @@
+import { useState } from "react";
+
+import MovieCard from "../components/movieCard/MovieCard.jsx";
+
 const FavoritePage = () => {
+  const [favoriteMovies, setFavoriteMovies] = useState(
+    Object.keys(localStorage)
+          .filter((key) => key.startsWith('movie_'))
+          .map((key) => JSON.parse(localStorage.getItem(key)))
+  );
+
+  const handleRemoveFavorite = (movieId) => {
+    const updatedFavorites = favoriteMovies.filter((movie) => movie.id !== movieId);
+    setFavoriteMovies(updatedFavorites);
+    localStorage.removeItem(`movie_${movieId}`);
+  };
+
   return (
-    <h1>FavoritePage</h1>
+    <div className="movie-container">
+      <h1>FavoritePage</h1>
+      <div className="movie-list">
+        {favoriteMovies.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} onRemoveFavorite={handleRemoveFavorite} />
+        ))}
+      </div>
+    </div>
   );
 };
 
