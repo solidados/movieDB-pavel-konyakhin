25168d582e454e4359692c76e1e222a7121f7d94
solidados <geekbrains.pk@internet.ru>
2024-01-12 17:15:48 +0400

    feat: add useEffect to App.jsx to switch between pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-import MainPage from "./pages/MainPage.jsx";
+import { useEffect, useState } from "react";
+
 import Header from "./components/header/Header.jsx";
+import MainPage from "./pages/MainPage.jsx";
+import FavoritePage from "./pages/FavoritePage.jsx";
 
 function App () {
+  const [page, setPage] = useState('MainPage')
+
+  useEffect(() => {
+    const path = window.location.pathname;
+    if (path === "/") {
+      setPage('MainPage');
+    } else if (path === "/favorites") {
+      setPage('FavoritePage');
+    }
+  }, []);
+
   return (
     <>
       <Header />
-      <MainPage />
+      {page === 'MainPage' && <MainPage />}
+      {page === 'FavoritePage' && <FavoritePage />}
     </>
   )
 }
