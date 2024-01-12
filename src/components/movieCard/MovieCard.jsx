06a1d7ee9649173582982c902d87befb65cc4c7f
solidados <geekbrains.pk@internet.ru>
2024-01-12 17:00:06 +0400

    feat: add favorite icon to MovieCard

--- src/components/movieCard/MovieCard.jsx
+++ src/components/movieCard/MovieCard.jsx
@@ -3,7 +3,7 @@ import MovieVote from "./ui/MovieVote.jsx";
 import MoviePoster from "./ui/MoviePoster.jsx";
 
 // styles
-import './styles/movieCard.scss'
+import './movieCard.scss'
 
 const MovieCard = ({ movie }) => {
   const IMG_URL = 'https://image.tmdb.org/t/p/w200';
@@ -17,9 +17,26 @@ const MovieCard = ({ movie }) => {
       <div className="card-poster">
         <MoviePoster moviePath={`${IMG_URL}${movie.poster_path}`} />
         <MovieVote voteAverage={movie.vote_average} />
+        <Favorite movie={movie} />
       </div>
     </div>
   );
 };
+
+const Favorite = ({ movie }) => {
+  let favorite = movie.favorite
+
+  return (
+    <form method="post">
+      <button
+        name="favorite"
+        value={favorite ? "false" : "true"}
+        aria-label={favorite ? "Remove from favorites" : "Add to favorites"}
+      >
+        {favorite ? "★" : "☆"}
+      </button>
+    </form>
+  )
+}
 
 export default MovieCard;
