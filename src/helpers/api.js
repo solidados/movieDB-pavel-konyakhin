9bb63a7e1077510efe1f837e5890d0f1d82fb903
solidados <geekbrains.pk@internet.ru>
2024-01-13 18:08:20 +0400

    chore: add api to fetch MovieDetails

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,9 +1,17 @@
 const API_KEY = '25803b30d68738b5daf05b883e47b5d5'
-// const API_URL = `https://api.themoviedb.org/3/movie/105?api_key=${API_KEY}`
 const API_URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
 // const API_URL = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`
 
 export const fetchedData = () => fetch(API_URL)
+  .then(res => {
+    if (!res.ok) {
+      throw new Error('Request failed! No Data')
+    }
+    return res.json()
+  })
+  .catch(err => console.error(err.message))
+
+export const fetchedMovie = (movieId) => fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}`)
   .then(res => {
     if (!res.ok) {
       throw new Error('Request failed! No Data')
