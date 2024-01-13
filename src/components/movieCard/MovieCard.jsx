e32346416b6e363343d877fd99f0df34ba790ced
solidados <geekbrains.pk@internet.ru>
2024-01-13 22:55:07 +0400

    feat: add to MovieCard logic to open and close the ModalWindow using current state

--- src/components/movieCard/MovieCard.jsx
+++ src/components/movieCard/MovieCard.jsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
+
 import MovieVote from "./ui/MovieVote.jsx";
 import MoviePoster from "./ui/MoviePoster.jsx";
+import MovieDetails from "../modal/MovieDetails.jsx";
 
 import './movieCard.scss'
 
 const MovieCard = ({ movie, onRemoveFavorite }) => {
+  const [isOpen, setIsOpen] = useState(false)
   const IMG_URL = 'https://image.tmdb.org/t/p/w200';
+
+  const openModal = () => {
+    setIsOpen(true)
+  }
+  const closeModal = () => {
+    setIsOpen(false)
+  }
 
   if (!movie) {
     return <div>Loading...</div>;
@@ -12,11 +23,16 @@ const MovieCard = ({ movie, onRemoveFavorite }) => {
 
   return (
     <div className="card">
-      <div className="card-poster">
+      <div className="card-poster" onClick={openModal}>
         <MoviePoster moviePath={`${IMG_URL}${movie.poster_path}`} />
         <MovieVote voteAverage={movie.vote_average} />
         <Favorite movie={movie} onRemoveFavorite={onRemoveFavorite} />
       </div>
+      {isOpen && (
+        <>
+          <MovieDetails movieId={movie.id} closeModal={closeModal} />
+        </>
+      )}
     </div>
   );
 };
