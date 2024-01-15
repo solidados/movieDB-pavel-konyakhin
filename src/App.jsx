2a30503966b4f9ebd25f7ac9925f30805ca37b46
solidados <geekbrains.pk@internet.ru>
2024-01-15 16:25:30 +0400

    refactor: App.jsx changed for ReactRouter; add Outlet where to render pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,11 @@
-import { useEffect, useState } from "react";
-
+import { Outlet } from "react-router-dom";
 import Header from "./components/header/Header.jsx";
-import MainPage from "./pages/MainPage.jsx";
-import FavoritePage from "./pages/FavoritePage.jsx";
 
 function App () {
-  const [page, setPage] = useState('MainPage')
-
-  useEffect(() => {
-    const path = window.location.pathname;
-    if (path === "/") {
-      setPage('MainPage');
-    } else if (path === "/favorites") {
-      setPage('FavoritePage');
-    }
-  }, []);
-
   return (
     <>
       <Header />
-      {page === 'MainPage' && <MainPage />}
-      {page === 'FavoritePage' && <FavoritePage />}
+      <Outlet />
     </>
   )
 }
