cc343aa73719036c6317a57c63546cd1eb53a305
solidados <geekbrains.pk@internet.ru>
2024-01-15 16:24:09 +0400

    feat: createBrowserRouter in main.js

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import App from './App.jsx'
+import MainPage from "./pages/MainPage.jsx";
+import FavoritePage from "./pages/FavoritePage.jsx";
+import ErrorPage from "./pages/ErrorPage.jsx";
 
 import './index.css'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/',
+        element: <MainPage />
+      },
+      {
+        path: '/favorites',
+        element: <FavoritePage />
+      }
+    ]
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
